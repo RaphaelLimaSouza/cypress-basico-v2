2d8018a8039654bce64d575cb1c3265e1ce72113
Raphael lima <phaelju@gmail.com>
2024-07-19 11:51:56 -0300

    Create cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
